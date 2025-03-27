d37abc597db56f84eff183da82d221f18a18a905
kugatheesan <vkugatheesan@gmail.com>
2025-03-27 21:53:35 +0530

    ssl change

--- src/database.ts
+++ src/database.ts
@@ -9,7 +9,8 @@ const pool=new Pool({
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
-    port: parseInt(String(process.env.DB_PORT))
+    port: parseInt(String(process.env.DB_PORT)),
+    ssl: { rejectUnauthorized: false }
 })
 
 const connectDB = async () => {
